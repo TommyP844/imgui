53bb7bcad90308a4c752d7f99f4894ccb105d951
TommyP844 <73144632+TommyP844@users.noreply.github.com>
2023-01-29 15:07:44 -0500

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,14 +1,35 @@
 project "ImGui"
 	language "C++"
-	kind "ConsoleApp"
+	kind "StaticLib"
 	location ""
 	cppdialect "C++17"
 	
 	files
 	{
+        "imgui.h",
+        "imgui_stdlib.h",
+        "imgui_config.h",
+        "imgui_internal.h",
+        "imstb_rectpack.h",
+        "imstb_truetype.h",
+        "imstb_textedit.h",
+
+        "imgui.cpp",
+        "imgui_freetype.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+
+        -- Backends
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp"
+
+        "backends/imgui_impl_vulkan.h",
+        "backends/imgui_impl_vulkan.cpp"
 	}
 
-		optimize "Speed"
-		floatingpoint "Fast"
-		intrinsics "On"
+	optimize "Speed"
+	floatingpoint "Fast"
+	intrinsics "On"
 	
