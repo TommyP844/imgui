008c34bdcc991ba5adfcbd3691942fedccd8f67e
Thomas Payne <tommypayne093@gmail.com>
2025-07-19 13:04:09 -0400

    Updated premake to include imgui_stdlib

--- premake5.lua
+++ premake5.lua
@@ -7,6 +7,11 @@ project "ImGui"
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir    ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
    
+    includedirs 
+    {
+        ""
+    }
+
 	files
 	{
         "imgui.h",
